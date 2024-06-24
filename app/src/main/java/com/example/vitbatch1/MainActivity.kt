9b610897f301b6194b6f6f945ed61d9946473e47
SETHROLLINSISMYGOAT <siddhantpathak128@gmail.com>
2024-06-24 09:40:24 +0530

    language

--- app/src/main/java/com/example/vitbatch1/MainActivity.kt
+++ app/src/main/java/com/example/vitbatch1/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.vitbatch1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.lang.reflect.Modifier
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
+
 }
--- app/src/main/java/com/example/vitbatch1/homeActivity.kt
+++ app/src/main/java/com/example/vitbatch1/homeActivity.kt
@@ -1,8 +1,11 @@
 package com.example.vitbatch1
 
+import android.content.ClipData.Item
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.AdapterView
+import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,14 +15,15 @@ import com.example.vitbatch1.database.ItemDao
 import com.example.vitbatch1.database.ItemRoomDatabase
 import com.example.vitbatch1.databinding.ActivityHomeBinding
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 
 class HomeActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener,
     AdapterView.OnItemClickListener {
-    class HomeActivity : AppCompatActivity() {
+    class HomeActivity<Spinner> : AppCompatActivity() {
         var TAG = HomeActivity::class.java.simpleName    //"HomeActivity"
-        lateinit var mySpinner: Spinner
-        lateinit var myListview:ListView
+
+        lateinit var myListview: ListView
         private lateinit var binding: ActivityHomeBinding
         lateinit var dao: ItemDao
 
@@ -27,25 +31,22 @@ class HomeActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener,
             super.onCreate(savedInstanceState)
             enableEdgeToEdge()
             setContentView(R.layout.activity_home)
-            mySpinner = findViewById(R.id.spinner) //taking handle
-            myListview = findViewById(R.id.listView)
+
             myListview.isClickable = true
             binding = ActivityHomeBinding.inflate(layoutInflater)
             val view = binding.root
             setContentView(view)
-            var  database = ItemRoomDatabase.getDatabase(this)
-            dao = database.itemDao()
+
+
 
             ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
                 val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
                 v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
                 insets
-                binding.btnDbInsert.setOnClickListener{
-                    insertDataDb()
+
                 }
 
-                mySpinner.onItemSelectedListener = this
-                myListview.setOnItemClickListener(this)
+
                 //get intent which sttarted this activity
                 //get the extras from that intent
                 //get the string with mykey
@@ -55,24 +56,4 @@ class HomeActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener,
                   var homeTextView:TextView = findViewById(R.id.tvHome)
                   homeTextView.setText(data)*/
             }
-
-            override fun onItemSelected(adpater: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                var item:String = adpater?.getItemAtPosition(position).toString()
-                Log.i(TAG, item )
-                private fun insertDataDb() {
-                    GlobalScope.launch {
-                        var item = Item(21,"fruits",11.11,11)
-                        dao.insert(item)
-                    }
-                }
-
-                override fun onNothingSelected(parent: AdapterView<*>?) {
-                    TODO("Not yet implemented")
-                }
-
-                override fun onItemClick(adpater: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                    var item:String = adpater?.getItemAtPosition(position).toString()
-                    Log.i(TAG, item )    }
-            }
-
 
