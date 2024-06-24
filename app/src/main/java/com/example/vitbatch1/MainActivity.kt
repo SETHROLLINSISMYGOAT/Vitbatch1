234967415184fc5e32478475a07348bd59b93e98
SETHROLLINSISMYGOAT <siddhantpathak128@gmail.com>
2024-06-24 17:26:08 +0530

    language

--- app/src/main/java/com/example/vitbatch1/MainActivity.kt
+++ app/src/main/java/com/example/vitbatch1/MainActivity.kt
@@ -2,31 +2,63 @@ package com.example.vitbatch1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
 import java.lang.reflect.Modifier
 
-class MainActivity : AppCompatActivity() {
+class MainActivity<Dp> : AppCompatActivity() {
+    private fun height(dp: Dp) {
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setContent {
+            TranslateAppTheme {
+                Box {
+                    Column(
+                        modifier = Modifier
+                            .fieldModifiers()
+                            .padding()
+                    )
 
+                }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        }
 
     }
+
+    fun Column(modifier: Modifier) {
+
+    }
+}
+
+    private fun Column(modifier: Modifier) {
+        TODO("Not yet implemented")
+    }
+
+    private fun Column(modifier: Modifier) {
+
+    }
+
+
+}
+
+private fun Int.padding(): Modifier {
+
+}
+
+private fun setContent(function: () -> Unit) {
+
+    }
+
+}
+
+class Colomn(modifier: Any, function: () -> Unit) {
+
+}
+
+class TranslateAppTheme(function: @Composable () -> Unit) {
 
 }
