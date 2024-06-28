e1a159aca1f302ba08f189586dc3a6b231be6fb0
SETHROLLINSISMYGOAT <siddhantpathak128@gmail.com>
2024-06-29 04:09:27 +0530

    signup screen

--- app/src/main/java/com/example/vitbatch1/MainActivity.kt
+++ app/src/main/java/com/example/vitbatch1/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.vitbatch1
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 
 
@@ -11,10 +15,26 @@ import androidx.appcompat.app.AppCompatActivity as AppCompatActivity1
 
 class MainActivity : AppCompatActivity1() {
     @SuppressLint("MissingInflatedId")
-    override fun onCreate(savedInstanceState: Bundle?) {
+    lateinit var usernameInput: EditText
+    lateinit var passwordInput: EditText
+    lateinit var loginBtn: Button
+
+    fun onCreate(savedInstanceState: Bundle?, msg: String) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        usernameInput = findViewById(R.id.username_input)
+        passwordInput = findViewById(R.id.password_input)
+        loginBtn = findViewById(R.id.login_btn)
+
+        loginBtn.setOnClickListener() { it: View ->
+            val usernamme = usernameInput.text.toString()
+            val password = passwordInput.text.toString()
+
+
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -22,4 +42,6 @@ class MainActivity : AppCompatActivity1() {
         }
     }
 }
+
+
 
